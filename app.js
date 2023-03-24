cfc66cb67f716d3a838491c9939de45bbc9a4283
Rasutei <rasutei@outlook.com>
2023-03-24 09:38:24 -0300

    Started working on itemized pages

--- app.js
+++ app.js
@@ -12,22 +12,20 @@ for (const page of document.querySelectorAll('main > .page')) {
 	Object.defineProperty(page, 'sidebar', {
 		get: function() {return this.querySelector('.sidebar')}})
 }
-const current = body.current
-const page = body.page
 
 //# ----------------------------------------
 //# Event Listeners
 //# ----------------------------------------
 //? Section pointer updates
 for (const page of document.querySelectorAll('main > .page'))
-	page.content.addEventListener('scroll', Page_RefreshSectionPointer)
+	page.content?.addEventListener('scroll', Page_RefreshSectionPointer)
 
 //# ----------------------------------------
 //# Section tracking
 //# ----------------------------------------
 function Page_GetCurrentSection() {
-	const height = page.content.offsetHeight
-	for (const child of page.content.children) {
+	const height = body.page.content.offsetHeight
+	for (const child of body.page.content.children) {
 		const rect = child.getBoundingClientRect()
 		if (rect.bottom >= height * .4)
 			return child
@@ -35,14 +33,16 @@ function Page_GetCurrentSection() {
 }
 function Page_GetCurrentSectionHeader() {
 	let section = Page_GetCurrentSection()
+	if (section.tagName == 'HEADER')
+		return body.page.content.querySelector('h1').textContent
 	for (;!section.tagName.match(/^h\d/i); section = section.previousElementSibling)
 		null
 	return section.textContent
 }
 function Page_RefreshSectionPointer() {
 	const headerText = Page_GetCurrentSectionHeader()
-	page.sidebar.querySelector('.current')?.classList.remove('current')
-	for (const child of page.sidebar.children)
+	body.page.sidebar.querySelector('.current')?.classList.remove('current')
+	for (const child of body.page.sidebar.children)
 		if (child.textContent == headerText) {
 			child.classList.add('current')
 			return
